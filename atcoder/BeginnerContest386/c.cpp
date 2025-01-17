9c1442b5f153d5d537a8f434b846d4fee0c81ea3
xyb7910 <1502709024@qq.com>
2025-01-17 11:24:05 +0800

    atcoder

--- atcoder/BeginnerContest386/c.cpp
+++ atcoder/BeginnerContest386/c.cpp
@@ -2,7 +2,7 @@
 * @Author: Yanpb
 * @Date:   2025-01-04 17:56:37
 * @Last Modified by:   Yanpb
-* @Last Modified time: 2025-01-06 19:39:37
+* @Last Modified time: 2025-01-17 10:26:59
 */
 #include <iostream>
 #include <cstring>
@@ -28,6 +28,44 @@ int dxr[8] = {-2, -1, 1, 2, 2, 1, -1, -2}, dyr[8] = {1, 2, 2, 1, -1, -2, -2, -1}
 
 void solved() {
 	/* your code */
+    int k; cin >> k;
+    string T, S; cin >> S >> T;
+    // 完全一样
+    if(T == S) {
+        puts("Yes");
+        return;
+    }
+    // 长度一样
+    int n = S.size(), m = T.size();
+    if(n == m) {
+        int diff = 0;
+        for (int i = 0; i < n; i ++) {
+            if(T[i] != S[i]) diff += 1;
+        }
+        if(diff <= 1) puts("Yes");
+        else puts("No");
+        return ;
+    }
+    // T 比 S 长 1
+    if(n == m - 1) {
+        swap(S, T);
+        swap(n, m);
+    }
+
+    if(n == m + 1) {
+        int pre = 0;
+        while(pre < m && S[pre] == T[pre]) ++ pre;
+        int suf = 0;
+        while(suf < m && S[n - suf - 1] == T[m - suf - 1]) ++ suf;
+        if(suf + pre >= n - 1) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+        return ;
+    }
+
+    puts("No");
 
 }
 

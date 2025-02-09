b03decc40a8f8ff6575845463fdf7afa92d5dc14
Yanpb <1502709024@qq.com>
2025-02-09 21:37:08 +0800

    noi

--- noi/2.1基本算法之枚举/完美立方.cpp
+++ noi/2.1基本算法之枚举/完美立方.cpp
@@ -2,24 +2,32 @@
 * @Author: Yanpb
 * @Date:   2025-02-08 17:09:16
 * @Last Modified by:   Yanpb
-* @Last Modified time: 2025-02-08 18:08:01
+* @Last Modified time: 2025-02-09 15:32:10
 */
 #include <iostream>
 #include <cstring>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 
-int n;
-
 int main() {
-    cin >> n;
-    for (int i = 1; i <= n; i ++)
-        for (int j = i; j <= n; j ++)
-            for (int k = j; k <= n; k ++) {
-                int a = i + j + k;
-                if(int(pow(i, 3)) + int(pow(j, 3)) + int(pow(k, 3)) == int(pow(a, 3)))
-                    printf("Cube = %d, Triple = (%d,%d,%d)\n", a, i, j, k);
+    int N;
+    cin >> N;
+
+    for (int a = 2; a <= N; ++a) {
+        for (int b = 2; b < a; ++b) {
+            for (int c = b; c < a; ++c) {
+                for (int d = c; d < a; ++d) {
+                    int a_cube = a * a * a;
+                    int b_cube = b * b * b;
+                    int c_cube = c * c * c;
+                    int d_cube = d * d * d;
+                    if (a_cube == b_cube + c_cube + d_cube) {
+                        cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ")" << endl;
+                    }
+                }
             }
+        }
+    }
+
     return 0;
 }
